e38433ba8195d160be4e3ba7f008ad9bc299069d
Paul Adams <ghusiyy@gmail.com>
2023-08-01 00:49:48 -0700

    make tests pass

--- patterns/sliding-window/index.test.ts
+++ patterns/sliding-window/index.test.ts
@@ -2,9 +2,9 @@ import { assertEquals } from "https://deno.land/std@0.113.0/testing/asserts.ts";
 import { longestSubstring } from "./index.ts";
 
 Deno.test("Sliding Window", () => {
-  assertEquals("abc", longestSubstring("abcabcbb"));
-  assertEquals("b", longestSubstring("bbbbb"));
-  assertEquals("wke", longestSubstring("pwwkew"));
-  assertEquals("ab", longestSubstring("aab"));
-  assertEquals("abcdefg", longestSubstring("abcdefg"));
+  assertEquals(3, longestSubstring("abcabcbb"));
+  assertEquals(1, longestSubstring("bbbbb"));
+  assertEquals(3, longestSubstring("pwwkew"));
+  assertEquals(2, longestSubstring("aab"));
+  assertEquals(7, longestSubstring("abcdefg"));
 });
--- patterns/sliding-window/index.ts
+++ patterns/sliding-window/index.ts
@@ -1,5 +1,18 @@
-function longestSubstring(str: string): string {
-  return str;
+function longestSubstring(str: string): number {
+  let start = 0;
+  let max = 0;
+  const map: { [key: string]: number } = {};
+
+  for (const [index, el] of str.split("").entries()) {
+    const curr = map[el];
+    if (curr !== undefined && curr >= start) {
+      start = curr + 1;
+    }
+    map[el] = index;
+    max = Math.max(max, index - start + 1);
+  }
+
+  return max;
 }
 
 export { longestSubstring };
