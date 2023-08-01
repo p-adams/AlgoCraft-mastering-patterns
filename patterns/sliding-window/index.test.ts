f0d035b267d20f3dc6eba22dce93ad9754691f6d
Paul Adams <ghusiyy@gmail.com>
2023-08-01 00:38:27 -0700

    add failing test cases // TODO: make tests pass

--- patterns/sliding-window/index.test.ts
+++ patterns/sliding-window/index.test.ts
@@ -2,5 +2,9 @@ import { assertEquals } from "https://deno.land/std@0.113.0/testing/asserts.ts";
 import { longestSubstring } from "./index.ts";
 
 Deno.test("Sliding Window", () => {
-  assertEquals("meow", longestSubstring("meow"));
+  assertEquals("abc", longestSubstring("abcabcbb"));
+  assertEquals("b", longestSubstring("bbbbb"));
+  assertEquals("wke", longestSubstring("pwwkew"));
+  assertEquals("ab", longestSubstring("aab"));
+  assertEquals("abcdefg", longestSubstring("abcdefg"));
 });
