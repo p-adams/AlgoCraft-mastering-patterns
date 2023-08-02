58167c9d87302951438839ef530409e1daa66c5f
Paul Adams <ghusiyy@gmail.com>
2023-08-01 18:30:53 -0700

    impl for minWindowSubstring

--- patterns/sliding-window/index.test.ts
+++ patterns/sliding-window/index.test.ts
@@ -1,10 +1,22 @@
 import { assertEquals } from "https://deno.land/std@0.113.0/testing/asserts.ts";
-import { longestSubstring } from "./index.ts";
+import { longestSubstring, minWindowSubstring } from "./index.ts";
 
-Deno.test("Sliding Window", () => {
+Deno.test("#longestSubstring", () => {
   assertEquals(3, longestSubstring("abcabcbb"));
   assertEquals(1, longestSubstring("bbbbb"));
   assertEquals(3, longestSubstring("pwwkew"));
   assertEquals(2, longestSubstring("aab"));
   assertEquals(7, longestSubstring("abcdefg"));
+});
+
+Deno.test("#minWindowSubstring", () => {
+  const input1 = "ADOBECODEBANC";
+  const input2 = "ABC";
+  const input3 = "aa";
+  const input4 = "aa";
+  const input5 = "ABCDE";
+  const input6 = "ABC";
+  assertEquals("BANC", minWindowSubstring(input1, input2));
+  assertEquals("aa", minWindowSubstring(input3, input4));
+  assertEquals("ABC", minWindowSubstring(input5, input6));
 });
--- patterns/sliding-window/index.ts
+++ patterns/sliding-window/index.ts
@@ -15,4 +15,54 @@ function longestSubstring(str: string): number {
   return max;
 }
 
-export { longestSubstring };
+function minWindowSubstring(s: string, t: string): string {
+  const frequencyMap = new Map<string, number>();
+
+  // Prepare the frequency map for characters in 't'.
+  for (const ch of t) {
+    frequencyMap.set(ch, (frequencyMap.get(ch) ?? 0) + 1);
+  }
+
+  let left = 0;
+  let right = 0;
+  let requiredCharacters = frequencyMap.size;
+  let minLength = Infinity;
+  let minWindowStart = 0;
+
+  while (right < s.length) {
+    const currentChar = s[right];
+
+    if (frequencyMap.has(currentChar)) {
+      frequencyMap.set(currentChar, frequencyMap.get(currentChar)! - 1);
+      if (frequencyMap.get(currentChar) === 0) {
+        requiredCharacters--;
+      }
+    }
+
+    while (requiredCharacters === 0) {
+      // Update 'minLength' and 'minWindowStart' if the current window is smaller.
+      if (right - left + 1 < minLength) {
+        minLength = right - left + 1;
+        minWindowStart = left;
+      }
+
+      const leftChar = s[left];
+      if (frequencyMap.has(leftChar)) {
+        frequencyMap.set(leftChar, frequencyMap.get(leftChar)! + 1);
+        if (frequencyMap.get(leftChar) === 1) {
+          requiredCharacters++;
+        }
+      }
+
+      left++;
+    }
+
+    right++;
+  }
+
+  return minLength === Infinity
+    ? ""
+    : s.substring(minWindowStart, minWindowStart + minLength);
+}
+
+export { longestSubstring, minWindowSubstring };
