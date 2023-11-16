53e5d243d42b7cea8fa03deb6503937365442576
Paul Adams <ghusiyy@gmail.com>
2023-11-16 01:01:28 -0800

    #islandPerimeter solution

--- warmups/index.ts
+++ warmups/index.ts
@@ -1421,7 +1421,23 @@ function countStudents(students: number[], sandwiches: number[]): number {
 }
 
 function islandPerimeter(grid: number[][]): number {
-  return -1;
+  const row = grid.length;
+  const cols = grid[0].length;
+  let perimeter = 0;
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (grid[i][j] === 1) {
+        perimeter += 4;
+        if (i > 0 && grid[i - 1][j] === 1) {
+          perimeter -= 2;
+        }
+        if (j > 0 && grid[i][j - 1] === 1) {
+          perimeter -= 2;
+        }
+      }
+    }
+  }
+  return perimeter;
 }
 
 export {
