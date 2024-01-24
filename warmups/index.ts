0c816ed022bf10c8561e6eecf024967c8bdddafb
Paul Adams <ghusiyy@gmail.com>
2024-01-23 18:12:54 -0800

    ##minimumCost: test passed

--- warmups/index.ts
+++ warmups/index.ts
@@ -2836,7 +2836,17 @@ function timeRequiredToBuy(tickets: number[], k: number): number {
 }
 
 function minimumCost(nums: number[]): number {
-  return -1;
+  let min1 = Infinity;
+  let min2 = Infinity;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] < min1) {
+      min2 = min1;
+      min1 = nums[i];
+    } else if (nums[i] < min2) {
+      min2 = nums[i];
+    }
+  }
+  return nums[0] + min1 + min2;
 }
 
 export {
