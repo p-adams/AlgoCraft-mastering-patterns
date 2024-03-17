d10c4a616c5ed88906e2e86337c79f561ef6a9dc
Paul Adams <ghusiyy@gmail.com>
2024-03-16 20:27:43 -0700

    #insert: test passed

--- warmups/sandbox1/index.ts
+++ warmups/sandbox1/index.ts
@@ -784,7 +784,30 @@ function judgeCircle(moves: string): boolean {
 }
 
 function insert(intervals: number[][], newInterval: number[]): number[][] {
-  return [];
+  const result: number[][] = [];
+  let i = 0;
+
+  // Add intervals before newInterval
+  while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+    result.push(intervals[i]);
+    i++;
+  }
+
+  // Merge overlapping intervals
+  while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+    newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+    newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+    i++;
+  }
+  result.push(newInterval);
+
+  // Add intervals after newInterval
+  while (i < intervals.length) {
+    result.push(intervals[i]);
+    i++;
+  }
+
+  return result;
 }
 
 export default {
